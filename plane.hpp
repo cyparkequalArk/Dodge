2e1559e0e09f838a86ad2fbf472f49ee12532a8e
Ahn,SeungRye <seungrye@gmail.com>
2016-02-20 15:26:56 +0900

    테스트 코드를 main.cpp 로 옮김.
    wrapper를 만들지 않고, 접근 한정자를 #define을 통해서 모두 public 으로 변경함.
    따라서 wrapper가 불필요함.

--- plane.hpp
+++ plane.hpp
@@ -14,6 +14,8 @@
 
 #if defined(_DEBUG)||!defined(NDEBUG)
 #define virtual_unit virtual
+#define protected	public
+#define private		public
 #endif
 
 class CPlane {
@@ -29,42 +31,10 @@ protected:
 	sf::Vector2f _speed;
 };
 
-namespace unit_test {
-	class CMockPlane1 : public CPlane {
-	public:
-		CMockPlane1(sf::Vector2f const& position) :
-			CPlane(position) {}
-	public: //utility
-		sf::Vector2f& getPosition() {
-			return _position;
-		}
-		sf::Vector2f& getSpeed() {
-			return _speed;
-		}
-	};
-	SCENARIO("비행기는 사용자 입력에 따라 움직여야 한다", "CPlane") {
-		GIVEN("화면상의 30, 30 위치에 비행기 객체가 만들어져 있다.") {
-			CMockPlane1 plane(sf::Vector2f(30, 30));
-			THEN("비행기의 위치를 확인시 30, 30 이어야 한다") {
-				sf::Vector2f pos = plane.getPosition();
-				REQUIRE(pos.x == 30.f);
-				REQUIRE(pos.y == 30.f);
-			}
-			THEN("비행기의 움직이는 속도는 0보다 높아야 한다") {
-				sf::Vector2f spd = plane.getSpeed();
-				REQUIRE(spd.x > 0.f);
-				REQUIRE(spd.y > 0.f);
-			}
-			WHEN("사용자가 <- 방향키를 눌렀다면") {
-				sf::Vector2f pos = plane.getPosition();
-				//plane.move(UserInput::Left);
-				THEN("비행기는 30, 30 위치보다 왼쪽으로 이동해 있어야 한다") {
-					sf::Vector2f position = plane.getPosition();
-					CHECK(position.x < pos.x);
-					CHECK(position.y == pos.y);
-				}
-			}
-		}
-	}
-}
+#if defined(_DEBUG)||!defined(NDEBUG)
+#undef virtual_unit
+#undef protected
+#undef private
+#endif
+
 #endif
